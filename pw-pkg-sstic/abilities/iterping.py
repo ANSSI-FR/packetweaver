1ea3dad8411ded0ec3db8dc9b2fb708161f1c194
sebastien mainand <sebastien.mainand@ssi.gouv.fr>
2018-03-29 16:13:33 +0200

    fix PrefixOpt handling when ipaddress/netaddr libs are not available

--- pw-pkg-sstic/abilities/iterping.py
+++ pw-pkg-sstic/abilities/iterping.py
@@ -1,6 +1,15 @@
 # coding: utf8
 import packetweaver.core.ns as ns
-import subprocess
+try:
+    import ipaddress
+    HAS_NET_LIB = True
+except ImportError:
+    try:
+        import netaddr
+        HAS_NET_LIB = True
+    except ImportError:
+        HAS_NET_LIB = False
+
 
 class Ability(ns.AbilityBase):
     _info = ns.AbilityInfo(
@@ -20,3 +29,11 @@ class Ability(ns.AbilityBase):
                 self.get_dependency('ping', ip_dst=target_ip).start()
         except StopIteration:
             pass
+
+    @classmethod
+    def check_preconditions(cls, module_factory):
+        l = []
+        if not HAS_NET_LIB:
+            l.append('Python ipaddress or netaddr is required to run this ability. Please install one of them.')
+        l += super(Ability, cls).check_preconditions(module_factory)
+        return l
