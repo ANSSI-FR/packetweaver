9788e3e8a96e292b5ef9aab363336ca1740f5481
sebastien mainand <sebastien.mainand@ssi.gouv.fr>
2018-05-25 16:12:01 +0200

    setup basic setup.py config

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='packetweaver',
+      version='0.2',
+      author='Sebastien Mainand',
+      author_email='sebastien.mainand@ssi.gouv.fr',
+      description='A Python framework for script filing and task sequencing',
+      url='https://github.com/ANSSI-FR/packetweaver',
+      py_modules=['packetweaver'],
+)
+
